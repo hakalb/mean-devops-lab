f2ed6ea31465071dd105dfbf15b22574e68d3071
Håkan Ströberg <hakan.stroberg@gmail.com>
2020-01-13 23:40:23 +0100

    Added todos application

---
+++ apps/todos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mean-devops-lab-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'todos';
+}
