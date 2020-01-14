9bf65572fd80721100decb6eedafecf2eefae846
Håkan Ströberg <hakan.stroberg@gmail.com>
2020-01-14 14:35:21 +0100

    Extend e2e test

--- apps/todos-e2e/src/integration/app.spec.ts
+++ apps/todos-e2e/src/integration/app.spec.ts
@@ -1,13 +1,24 @@
 import { getGreeting } from '../support/app.po';
+import { getAddTodoButton, getTodos } from '../support/app.po';
 
-describe('todos', () => {
+describe('todos header', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display My Todos', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to todos!');
+    getGreeting().contains('My Todos');
+  });
+});
+
+describe('TodoApps', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display todos', () => {
+    getTodos().should(t => expect(t.length).equal(2));
+    getAddTodoButton().click();
+    getTodos().should(t => expect(t.length).equal(3));
   });
 });
--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  title: String;
+}
+
 @Component({
   selector: 'mean-devops-lab-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todos';
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`
+    });
+  }
 }
